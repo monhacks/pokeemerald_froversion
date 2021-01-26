ab72a7149c61ec598daf07c5a1bfa627dcf0bc29
U-LAPTOP-212K00JA\Nicole <heracross1991@gmail.com>
2021-01-26 14:20:02 -0500

    did tilesets up to route 110

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -200,7 +200,7 @@
 #define METATILE_General_BlueCaveIndent        0x1B0
 #define METATILE_General_BlueCaveOpen          0x1B1
 #define METATILE_General_CalmWater             0x170
-#define METATILE_General_CaveEntrance_Bottom   0x0A7
+#define METATILE_General_CaveEntrance_Bottom   0x135
 #define METATILE_General_CaveEntrance_Top      0x09F
 #define METATILE_General_Door                  0x021
 #define METATILE_General_Door_Contest          0x1DB
