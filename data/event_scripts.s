2b49a091d8b48e51199ee20a391b1f8cf664cb30
U-LAPTOP-212K00JA\Nicole <heracross1991@gmail.com>
2021-07-04 08:32:03 -0400

    mugshots added

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -39,6 +39,7 @@
 #include "constants/mauville_old_man.h"
 #include "constants/metatile_labels.h"
 #include "constants/moves.h"
+#include "constants/mugshots.h"
 #include "constants/party_menu.h"
 #include "constants/pokemon.h"
 #include "constants/roulette.h"
