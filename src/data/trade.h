018ee203a43e15845b1dcb9190db21e9668794dc
U-LAPTOP-212K00JA\Nicole <heracross1991@gmail.com>
2021-08-18 11:03:36 -0400

    dialouge changes up to rainree island

--- src/data/trade.h
+++ src/data/trade.h
@@ -981,18 +981,18 @@ static const struct InGameTrade sIngameTrades[] =
     [INGAME_TRADE_SEEDOT] = 
     {
         .nickname = _("DOTS"), 
-        .species = SPECIES_SEEDOT,
-        .ivs = {5, 4, 5, 4, 4, 4},
+        .species = SPECIES_MEDITITE,
+        .ivs = {15, 14, 15, 14, 14, 14},
         .abilityNum = 1, 
         .otId = 38726,
         .conditions = {30, 5, 5, 5, 5},
         .personality = 0x84,
         .heldItem = ITEM_CHESTO_BERRY, 
         .mailNum = -1,
-        .otName = _("KOBE"), 
+        .otName = _("Kant"), 
         .otGender = MALE, 
         .sheen = 10,
-        .requestedSpecies = SPECIES_RALTS
+        .requestedSpecies = SPECIES_MARRILL
     }, 
     [INGAME_TRADE_PLUSLE] = 
     {
