eab33474902448e9b5df81e48dafabb71f0c8c3d
Robert <heracross1991@gmail.com>
2021-09-25 06:47:10 -0400

    add Genetically Modified Pokemon Battle theme

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -553,6 +553,7 @@
 #define MUS_FV_TEAM_MAGA                       617
 #define MUS_FV_GOODNIGHT                       618
 #define MUS_FV_INTRO                           619
+#define MUS_FV_VS_GMP                          620
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
