6fdc7c38ecee153ab12c6ff20b86b928a3821351
Aneromp <anercomp@gmail.com>
2022-06-18 14:40:11 +0200

    Fixed Metatile behaviour for doors on CinnabarIsland

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -5,11 +5,7 @@
 // DO NOT MODIFY THIS FILE! It is auto-generated from data/maps/map_groups.json
 //
 
-<<<<<<< HEAD
-// Map Group 0
-=======
 // gMapGroup0
->>>>>>> 3afc504e75e7303d50fb528b65b94a6fb1c0b617
 #define MAP_PETALBURG_CITY                                       (0 | (0 << 8))
 #define MAP_SLATEPORT_CITY                                       (1 | (0 << 8))
 #define MAP_MAUVILLE_CITY                                        (2 | (0 << 8))
@@ -730,7 +726,5 @@
 #define MAP_NEW_MAP3                               (22 | (33 << 8))
 
 #define MAP_GROUPS_COUNT 34
-
-// static const u8 MAP_GROUP_COUNT[] = {129, 10, 10, 20, 8, 9, 9, 7, 8, 14, 8, 20, 10, 23, 14, 20, 15, 2, 2, 2, 3, 1, 1, 1, 108, 61, 89, 2, 1, 13, 1, 1, 3, 23, 0};
 
 #endif // GUARD_CONSTANTS_MAP_GROUPS_H
