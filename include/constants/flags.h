61624defa42b041cff08b24404d17b729adf479b
U-LAPTOP-212K00JA\Nicole <heracross1991@gmail.com>
2022-07-07 23:08:27 -0400

    final event script 90% done

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -253,7 +253,7 @@
 #define FLAG_RECEIVED_TM05                   0xE7
 #define FLAG_RECEIVED_TM19                   0xE8
 
-#define FLAG_UNUSED_0x0E9                    0xE9 // Unused Flag
+#define FLAG_HIDE_LAB_ITEM_BALL                    0xE9 // Unused Flag
 
 #define FLAG_RECEIVED_TM44                   0xEA
 #define FLAG_RECEIVED_TM45                   0xEB
