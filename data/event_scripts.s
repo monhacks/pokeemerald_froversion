634f97510f2b8b5cf94b76a20ea7d21bd31c191b
U-LAPTOP-212K00JA\Nicole <heracross1991@gmail.com>
2023-12-29 21:25:18 -0500

    added warps to preinfection maps. Added new mushrooms pre infection. Need to fix door palette

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1403,3 +1403,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/maps/MAGM8ProtagonistsHouse/scripts.inc"
 
 	.include "data/maps/MAGM8BlackForestPreInfection/scripts.inc"
+
+	.include "data/maps/MAGM8PreInfectionLostWoods/scripts.inc"
