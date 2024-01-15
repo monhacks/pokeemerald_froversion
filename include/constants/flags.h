8cf2dd272a01bdb3c5645e6a403c628cfb564412
Aneromp <anercomp@gmail.com>
2024-01-15 01:56:53 +0100

    Finishing touches and Credits
    Polished the maps and added Credits right after Ariados and in the protagonists house

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -454,7 +454,7 @@
 #define FLAG_REMATCH_DRAKE                   0x1A8
 #define FLAG_REMATCH_WALLACE                 0x1A9
 
-#define FLAG_UNUSED_0x1AA                    0x1AA // Unused Flag
+#define FLAG_LOST_BOSS_BATTLE                0x1AA // Lost to Boss Ariados
 #define FLAG_UNUSED_0x1AB                    0x1AB // Unused Flag
 
 #define FLAG_DEFEATED_DEOXYS                 0x1AC
