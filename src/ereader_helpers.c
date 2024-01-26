pret/pokeemerald

Static assertion for size of save structs

src/ereader_helpers.c
@@ -15,6 +15,8 @@
 #include "constants/items.h"
 #include "constants/trainer_hill.h"
 
+STATIC_ASSERT(sizeof(struct TrainerHillChallenge) <= SECTOR_DATA_SIZE, TrainerHillChallengeFreeSpace);
+
 struct SendRecvMgr
 {
     bool8 isParent;

monhacks/pokeemerald_froversion

src/ereader_helpers.c
@@ -15,6 +15,8 @@
 #include "constants/moves.h"
 #include "constants/items.h"
 
+STATIC_ASSERT(sizeof(struct TrHillTag) <= SECTOR_DATA_SIZE, TrainerHillChallengeFreeSpace);
+
 struct Unknown030012C8
 {
     u8 unk0[8];