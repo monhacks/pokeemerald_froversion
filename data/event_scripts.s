9b6f7e3e0802fa48cc5f5970285a17033a05041a
Heracross1991 <heracross1991@gmail.com>
2025-01-18 16:41:05 -0500

    added riverton town on ow map

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1437,3 +1437,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/maps/SaltLakeCityHouse1/scripts.inc"
 
 	.include "data/maps/SaltLakeCityHouse2/scripts.inc"
+
+	.include "data/maps/Riverton_Town/scripts.inc"
